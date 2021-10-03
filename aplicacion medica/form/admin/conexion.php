36ac1d3a98c044719e9cc44e8d04d5708d9cf04f
kimberly284 <60859487+kimberly284@users.noreply.github.com>
2021-10-02 21:00:26 -0600

    Delete aplicacion medica/form directory

--- aplicacion medica/form/admin/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBNAME', 'multimedica');
-
-
-$conexion = mysqli_connect(DBHOST, DBUSER, '', DBNAME);
--- aplicacion medica/form/js/script.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery('#data').DataTable();
-});
-
-
-
